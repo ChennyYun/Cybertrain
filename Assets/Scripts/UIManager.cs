dd42fae060df9ea7681aacf1ec1d2b897904de99
Cody Jiang <zj2247@nyu.edu>
2023-10-10 17:05:26 -0400

    THINGS DONE

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void StartGame(){
+        GameManager.Instance.StartGame();
+    }
+}
